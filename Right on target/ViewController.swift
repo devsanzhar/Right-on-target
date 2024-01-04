619d30e82e66de551ced99759ab99f25c32595ac
Sanzhar Serikbay <126369323+devsanzhar@users.noreply.github.com>
2024-01-04 15:59:37 +0600

    deleted all self

--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -26,48 +26,46 @@ class ViewController: UIViewController {
         
     }
     
-    
-    
     @IBAction func checkNumber() {
         // если игра только начинается
-        if self.round == 0 {
+        if round == 0 {
             // генерируем случайное число
-            self.number = Int.random(in: 1...50)
+            number = Int.random(in: 1...50)
             // передаем значение случайного числа в label
-            self.label.text = String(self.number)
+            label.text = String(number)
             // устанавливаем счетчик раундов на 1
-            self.round = 1
+            round = 1
         } else {
             // получаем значение на слайдере
-            let numSlider = Int(self.slider.value.rounded()) // сравниваем значение с загаданным
+            let numSlider = Int(slider.value.rounded()) // сравниваем значение с загаданным
             
             // и подсчитываем очки
-            if numSlider > self.number {
-                self.points += 50 - numSlider + self.number
+            if numSlider > number {
+                points += 50 - numSlider + number
             }
-            else if numSlider < self.number {
-                self.points += 50 - self.number + numSlider
+            else if numSlider < number {
+                points += 50 - number + numSlider
             }
             else {
-                self.points += 50
+                points += 50
             }
             
-            if self.round == 5 {
+            if round == 5 {
                 // выводим информационное окно // с результатами игры
                 let alert = UIAlertController(
                     title: "Игра окончена",
-                    message: "Вы заработали \(self.points) очков", preferredStyle: .alert)
+                    message: "Вы заработали \(points) очков", preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
-                self.present(alert, animated: true, completion: nil)
-                self.round = 1
-                self.points = 0
+                present(alert, animated: true, completion: nil)
+                round = 1
+                points = 0
             } else {
-                self.round += 1
+                round += 1
             }
             // генерируем случайное число
-            self.number = Int.random(in: 1...50)
+            number = Int.random(in: 1...50)
             // передаем значение случайного числа в label
-            self.label.text = String(self.number)
+            label.text = String(number)
         }
     }
 }
